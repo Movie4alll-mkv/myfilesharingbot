3e1f237057a6ba796d6e8e4abf5290b943cb3fcf
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2025-01-02 14:43:20 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,6 +19,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 # Define the channel IDs
 FORCE_SUB_CHANNEL_IDS = {
     "channel": "animecolony_1",
+    "channel9": "Sakamoto_days_englishdubbed",
     "channel2": "sololeveling_english_dubbed",
     "channel3": "rezero_english_dubb",
     "channel4": "myhappymarriagex",
@@ -36,7 +37,7 @@ async def start_command(client: Client, message: Message):
                 return
         except UserNotParticipant:
             await message.reply_text(
-                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @sololeveling_english_dubbed \n @RascalDoesNotDreamofBunnyGirl_1 \n @arifureta_english_dubbed_1 \n @myhappymarriagex \n @the100Girlfriendswhoreallyloveyd \n @bluelockdubenglishdubbed \n @animecolony_1 \n ",
+                text="Thanks for Joining ☺️ \n @animedualaudiozippercartoonist but You are not Subscribed to these Channels Join these channels and Support our Anime Channel and our Anime service 😀 thanks and Enjoy Watching Anime \n @sololeveling_english_dubbed \n @Sakamoto_days_englishdubbed \n @arifureta_english_dubbed_1 \n @myhappymarriagex \n @the100Girlfriendswhoreallyloveyd \n @bluelockdubenglishdubbed \n @animecolony_1 \n ",
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton("Update Channel", url=f"t.me/{sub_channel_id}")
