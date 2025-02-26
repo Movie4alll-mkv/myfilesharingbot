e676975337c30eb6bd302f4391eaadeb0f0c274b
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2025-02-26 19:31:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7889622689:AAHFFappBwSJyYUYzHXFdCfNPqekv1NMCxg")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "6534707"))
+APP_ID = int(os.environ.get("APP_ID", "13146696057"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
+API_HASH = os.environ.get("API_HASH", "099108f13146696057e9b83869566096")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002381359926"))
