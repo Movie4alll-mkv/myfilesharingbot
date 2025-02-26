dc9f8ed30bdef0311161d2080cc568b5715baf6f
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2025-02-26 19:44:36 +0500

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7889622689:AAHFFappBwSJyYUYzHXFdC
 APP_ID = int(os.environ.get("APP_ID", "6534707"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a"")
+API_HASH = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002246141753"))
